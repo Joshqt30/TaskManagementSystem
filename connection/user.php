6908649fa0afbb30c109b7aef8945101b8b54bf4
Joshqt30 <randomparaokay@gmail.com>
2025-04-04 23:45:47 +0800

    php mailer otw

---
+++ connection/user.php
@@ -0,0 +1,16 @@
+<?php
+// Database connection details
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'tms_db  ';
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
